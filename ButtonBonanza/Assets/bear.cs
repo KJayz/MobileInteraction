9404674297cd51eba83bbc540901314937225cd7
evwor <opdrachtenEMM@gmail.com>
2021-05-27 15:21:11 +0200

    improved speed and jumping, improved discrete lanes, tried to improve code quality of bear

--- ButtonBonanza/Assets/bear.cs
+++ ButtonBonanza/Assets/bear.cs
@@ -6,37 +6,54 @@ public class bear : MonoBehaviour
 {
 	int lane;
 	int laneMovement;
-    float startPosY;
-    float strafeSpeed = 2f;
-    float jumpSpeed = 2f;
+    float speed = 2f;
+    float strafeSpeed, jumpSpeed, duckSpeed;
 
-	Vector3 velocity = Vector3.zero;
 	public Material bearMat; 
 	public Material bearDuckMat;
+
 
     // Start is called before the first frame update
     void Start()
     {
     	lane = 0; // (negative = left lane, 0 = middle lane, positive = right lane)
-        startPosY = transform.position.y;
+    	duckSpeed = speed;
+    	jumpSpeed = speed;
+    	strafeSpeed = 2*speed;
     }
 
     // Update is called once per frame
     void Update()
+    {      
+        // Reset position & speed, if bear's position too low or too much to one of the sizes
+		resetPosition();
+
+		// respond to user's input
+        respondToInput();
+    }
+    
+    
+    void resetPosition()
     {
-        // Reset position & speed if cube is on track again
-        if (transform.position.y < 0.5f)
+    	// reset pos after jump
+    	if (transform.position.y < 0.5f)
         {
             transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             Debug.Log("Reset pos");
         }
-
-        if (GetComponent<Rigidbody>().velocity.y != 0)
+        
+        // reset pos for lanes, to keep the lanes discrete
+        if (transform.position.x < -1f || transform.position.x > 1f) 
         {
-            // Simulate Gravity
-            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity - jumpSpeed * Vector3.up * Time.deltaTime;
+            transform.position = new Vector3((float)lane, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Debug.Log("Reset pos");
         }
+    }
+    
+    void respondToInput()
+    {
         if(GetComponent<Rigidbody>().velocity == Vector3.zero)
         {
             if (Input.GetKey("a") && lane >= 0)
@@ -44,39 +61,49 @@ public class bear : MonoBehaviour
                 laneMovement = -1;
                 StartCoroutine(laneChange());
             }
+            
             if (Input.GetKey("d") && lane <= 0)
             {
-                laneMovement = +1;
+                laneMovement = 1;
                 StartCoroutine(laneChange());
             }
 
             if (Input.GetKey("w"))
-            {
-                GetComponent<Rigidbody>().velocity = Vector3.up * jumpSpeed;
-            }
+                StartCoroutine(jump());
             
             if (Input.GetKey("s"))
-            {
 				StartCoroutine(duck());
-            }
         }
     }
     
     IEnumerator laneChange()
     {
         GetComponent<Rigidbody>().velocity = new Vector3(laneMovement, 0, 0) * strafeSpeed;
+        lane += laneMovement;
         yield return new WaitForSeconds(1 / strafeSpeed);
         GetComponent<Rigidbody>().velocity = Vector3.zero;
-        lane += laneMovement;
     	laneMovement = 0;
+    }
+    
+    IEnumerator jump()
+    {
+    	GetComponent<Rigidbody>().velocity = jumpSpeed * Vector3.up;
+    	GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity - jumpSpeed * Vector3.up * Time.deltaTime;
+    	yield return new WaitWhile(() => transform.position.y < 1.5f);
+    	GetComponent<Rigidbody>().velocity = jumpSpeed * Vector3.down;
+    	GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity + jumpSpeed * Vector3.down * Time.deltaTime; 
+    	yield return new WaitWhile(() => transform.position.y > 0.5f); 
     }
     
     IEnumerator duck()
     {
+    	// bear starts ducking
         GetComponent<MeshRenderer>().material = bearDuckMat;
         GetComponent<BoxCollider>().size = new Vector3(8,0.075f,4.5f);
         GetComponent<BoxCollider>().center = new Vector3(0,0,2.7f);
-        yield return new WaitForSeconds(1.5f / strafeSpeed);
+        yield return new WaitForSeconds(1.5f / duckSpeed);
+        
+        // bear starts moving normally again
         GetComponent<MeshRenderer>().material = bearMat;
         GetComponent<BoxCollider>().size = new Vector3(8,0.075f,9.5f);
         GetComponent<BoxCollider>().center = new Vector3(0,0,0);
