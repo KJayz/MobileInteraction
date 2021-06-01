00b306fba33514d61bb10db44cd06797bdaf2c12
KJayz <kwintenjacobs@gmail.com>
2021-06-02 01:22:38 +0200

    Initial Firebase connection in scores.cs (crashes unity on pc, works on phone)

--- ButtonBonanza/Assets/scores.cs
+++ ButtonBonanza/Assets/scores.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Firestore;
+using Firebase.Extensions;
 
 public class scores : MonoBehaviour
 {
@@ -11,17 +13,54 @@ public class scores : MonoBehaviour
 	public static int nrPlayedLevels = 0;
     public static float timeSinceLevelLoad;
 
+
     // Start is called before the first frame update
     void Start()
     {
         playerScore = 0;
         timeSinceLevelLoad = Time.timeSinceLevelLoad;
 
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        submitToDB();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Debug.Log(playerScore);
+    }
+
+    static void submitToDB()
+    {
+
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+
+        DocumentReference docRef = db.Collection("users").Document("alovelace");
+        Dictionary<string, object> user = new Dictionary<string, object>
+{
+        { "First", "Ada" },
+        { "Last", "Lovelace" },
+        { "Born", 1815 },
+};
+        docRef.SetAsync(user).ContinueWithOnMainThread(task => {
+            Debug.Log("Added data to the alovelace document in the users collection.");
+        });
     }
 }
